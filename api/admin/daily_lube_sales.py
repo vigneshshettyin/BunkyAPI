0d1eb39985f515828bfc97165f4fb848db95bc65
Vignesh Shetty <vigneshshetty.in@gmail.com>
2024-12-19 23:40:20 +0530

    Admin for DailyLubeSales

--- api/admin/daily_lube_sales.py
+++ api/admin/daily_lube_sales.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from api.models import DailyLubeSales
 
+class DailyLubeSalesAdmin(admin.ModelAdmin):
+    list_display = (
+        "product",
+        "quantity",
+        "product_price",
+        "total_price",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("product__code", "created_at", "updated_at")
+    search_fields = ["product__code"]
 
-admin.site.register(DailyLubeSales)
+    def total_price(self, obj):
+        return f"₹{obj.quantity * obj.product.price}"
+    
+    def product_price(self, obj):
+        return f"₹{obj.product.price}"
+
+admin.site.register(DailyLubeSales, DailyLubeSalesAdmin)
